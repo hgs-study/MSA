7d8e497df5dd164984ad6d2b35508c6ed58af293
hgs-study <hgstudy_@naver.com>
2021-06-07 22:58:54 +0900

    [SpringCloud-Gateway] Filter 설정#2 (application.yml 활용)

--- api-gateway-service/src/main/java/com/hgstudy/apigatewayservice/config/FilterConfig.java
+++ api-gateway-service/src/main/java/com/hgstudy/apigatewayservice/config/FilterConfig.java
@@ -5,10 +5,10 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class FilterConfig {
 
-    @Bean
+//    @Bean
     public RouteLocator getawayRoutes(RouteLocatorBuilder builder){
         return builder.routes()
                 .route( r -> r.path("/first-service/**")

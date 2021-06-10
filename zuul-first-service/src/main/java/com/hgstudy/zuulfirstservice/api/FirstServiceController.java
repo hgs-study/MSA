2076728b24dbb1e99be4ac8d4ac15f0db5989f70
hgs-study <hgstudy_@naver.com>
2021-06-11 00:06:32 +0900

    [SpringCloud-Gateway] Load Balancer 설정#2

--- zuul-first-service/src/main/java/com/hgstudy/zuulfirstservice/api/FirstServiceController.java
+++ zuul-first-service/src/main/java/com/hgstudy/zuulfirstservice/api/FirstServiceController.java
@@ -1,10 +1,14 @@
 package com.hgstudy.zuulfirstservice.api;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 // http://localhost:8081/welcome
 // http://localhost:8081/firest-service/welcome
@@ -12,6 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/first-service")
 @Slf4j
 public class FirstServiceController {
+
+    Environment env;
+
+    public FirstServiceController(Environment env){
+        this.env =env;
+    }
 
     @GetMapping("/welcome")
     public String welcome(){
@@ -25,8 +35,10 @@ public class FirstServiceController {
     }
 
     @GetMapping("/check")
-    public String check(){
-        return "Hi, there. This is a message from First Service.";
+    public String check(HttpServletRequest request){
+        log.info("Server port = {}",request.getServerPort());
+        return String.format("Hi, there. This is a message from First Service. on PORT %s"
+                , env.getProperty("local.server.port"));
     }
 }
 

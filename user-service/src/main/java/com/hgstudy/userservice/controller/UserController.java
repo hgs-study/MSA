5cf830f6da7a990997812b0e482edb738c285ea8
hgs-study <hgstudy_@naver.com>
2021-06-24 00:21:26 +0900

    [Users Microservice] Routes 정보 변경

--- user-service/src/main/java/com/hgstudy/userservice/controller/UserController.java
+++ user-service/src/main/java/com/hgstudy/userservice/controller/UserController.java
@@ -20,7 +20,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/user-service/")
+@RequestMapping("/")
 @RequiredArgsConstructor
 public class UserController {
 
